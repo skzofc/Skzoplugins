ca393e705783594484f8b6bd52b5930d1e9c2023
skz Ofc <122362873+skzofc@users.noreply.github.com>
2025-06-21 17:12:41 +0800

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,27 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
+// use an integer for version numbers
+version = 10
 
-// Use an integer for version numbers
-version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
+    language = "id"
+    // All of these properties are optional, you can safely remove them
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
+    // description = "Lorem Ipsum"
+     authors = listOf("Hexated")
 
     /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
 
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+    iconUrl = "https://aghanim.xyz/wp-content/themes/animesail/assets/images/ico.png"
 }
